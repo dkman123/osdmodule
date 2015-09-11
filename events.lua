3cb82a0f3aba02060806126cf6408e137e9c6815
dk <derek.kelly@normantech.com>
2015-09-10 23:47:31 -0400

    finally figured out how to detect leaving

--- events.lua
+++ events.lua
@@ -108,64 +108,55 @@ local TalkStatus = {
 ]]
 end
 
-local function onConnectStatusChangeEvent(serverConnectionHandlerID, status, errorNumber)
---[[
-local ConnectStatus = {
-	STATUS_DISCONNECTED = 0,            -- There is no activity to the server, this is the default value
-	STATUS_CONNECTING = 1,              -- We are trying to connect, we haven't got a clientID yet, we haven't been accepted by the server
-	STATUS_CONNECTED = 2,               -- The server has accepted us, we can talk and hear and we got a clientID, but we don't have the channels and clients yet, we can get server infos (welcome msg etc.)
-	STATUS_CONNECTION_ESTABLISHING = 3, -- we are CONNECTED and we are visible
-	STATUS_CONNECTION_ESTABLISHED = 4   -- we are CONNECTED and we have the client and channels available
-}
-]]
+-------------------------------------------------------------------------------------
 
-    --ts3.printMessageToCurrentTab("osdModule: onConnectStatusChangeEvent: " .. serverConnectionHandlerID .. " " .. status .. " " .. errorNumber)
-	--os.execute("echo \"DEBUG TeamSpeak connect event " .. status .. "\" >> ~/Documents/ts3debug.log")
+local function onClientMoveEvent(serverConnectionHandlerID, clientID, oldChannelID, newChannelID, visibility, moveMessage)
+	--ts3.printMessageToCurrentTab("osdModule: onClientMoveEvent: srv=" .. serverConnectionHandlerID .. " client=" .. clientID .. " old=" .. oldChannelID .. " new=" .. newChannelID)
+	--os.execute("echo \"DEBUG TeamSpeak onClientMoveEvent event " .. oldChannelID .. ", " .. newChannelID .. "\" >> ~/Documents/ts3debug.log")
 
+	-- get my clientID	
 	local myClientID, error = ts3.getClientID(serverConnectionHandlerID)
-	-- when disconnecting from the server kill the window (otherwise it will sit there on top until manually killed)
-	-- if getClientID returns a value then someone else disconnected, so don't kill the window
-	--ts3.printMessageToCurrentTab("osdModule: onConnectStatusChangeEvent: status=" .. status .. ", errorNumber=" .. errorNumber .. ", local error=" .. error .. ", myClientID=" .. myClientID)
-	if (status==0 and myClientID==0) then
+
+
+	-- if I left the server then close the window
+	--ts3.printMessageToCurrentTab("osdModule: onClientMoveEvent: myClientID=" .. myClientID .. ", clientID=" .. clientID .. ", newChannelID=" .. newChannelID .. ", moveMessage=" .. moveMessage)
+	if (myClientID==0 and newChannelID==0) then
+		--ts3.printMessageToCurrentTab("osdModule: onClientMoveEvent: killing OSD")
 		os.execute("pkill -TERM -f \"dzen2.*TeamSpeak\"")
 		return
-	else
+	elseif (myClientID==clientID) then
+		-- else if I moved 
+	
 		-- Get name of this channel
-		local myChannelID, error = ts3.getChannelOfClient(serverConnectionHandlerID, myClientID)
-		local myChannelName, error = ts3.getChannelVariableAsString(serverConnectionHandlerID, myChannelID, ts3defs.ChannelProperties.CHANNEL_NAME)
+		local myChannelName, error = ts3.getChannelVariableAsString(serverConnectionHandlerID, newChannelID, ts3defs.ChannelProperties.CHANNEL_NAME)
 		if error == ts3errors.ERROR_ok and useChannelName==1 then
 			channelName = myChannelName
 		else
 			channelName = "TeamSpeak"
 		end
-	end
-end
 
-local function onClientMoveEvent(serverConnectionHandlerID, clientID, oldChannelID, newChannelID, visibility, moveMessage)
-	--ts3.printMessageToCurrentTab("osdModule: onChannelMoveEvent: " .. serverConnectionHandlerID .. " " .. oldChannelID .. " " .. newChannelID)
-	--os.execute("echo \"DEBUG TeamSpeak onClientMoveEvent event " .. oldChannelID .. ", " .. newChannelID .. "\" >> ~/Documents/ts3debug.log")
-	
-	-- Get name of this channel
-	local myChannelName, error = ts3.getChannelVariableAsString(serverConnectionHandlerID, newChannelID, ts3defs.ChannelProperties.CHANNEL_NAME)
-	if error == ts3errors.ERROR_ok and useChannelName==1 then
-		channelName = myChannelName
-	else
-		channelName = "TeamSpeak"
-	end
-
-	-- clear the client list
-	for key in pairs (clients) do
-		clients[key] = nil
-	end
-
-	-- Get the list of users in the channel
-	local clientList, error = ts3.getChannelClientList(serverConnectionHandlerID, newChannelID)
-	for i=1, #clientList do
-		local clientName, error = ts3.getClientVariableAsString(serverConnectionHandlerID, clientList[i], ts3defs.ClientProperties.CLIENT_NICKNAME)
-		if error == ts3errors.ERROR_ok then
-			clients[clientList[i]]=0;
+		-- clear the client list
+		for key in pairs (clients) do
+			clients[key] = nil
 		end
-	end	
+
+		-- Get the list of users in the channel
+		local clientList, error = ts3.getChannelClientList(serverConnectionHandlerID, newChannelID)
+		for i=1, #clientList do
+			local clientName, error = ts3.getClientVariableAsString(serverConnectionHandlerID, clientList[i], ts3defs.ClientProperties.CLIENT_NICKNAME)
+			if error == ts3errors.ERROR_ok then
+				clients[clientList[i]]=0;
+			end --if
+		end --for
+	end --else if
+	
+	-- get the moved client's name
+	local clientName, error = ts3.getClientVariableAsString(serverConnectionHandlerID, clientID, ts3defs.ClientProperties.CLIENT_NICKNAME)
+	if error == ts3errors.ERROR_ok then
+		--ts3.printMessageToCurrentTab("osdModule: onClientMoveEvent: clientName=" .. clientName)
+		clients[myClientID] = 0;
+	end
+
 end
 
 --
@@ -183,11 +174,59 @@ local function onMenuItemEvent(serverConnectionHandlerID, menuType, menuItemID, 
 	--os.execute("echo \"DEBUG TeamSpeak menu event\" >> ~/Documents/ts3debug.log")
 end
 
+--[[
+local function onConnectStatusChangeEvent(serverConnectionHandlerID, status, errorNumber)
+--[[
+local ConnectStatus = {
+	STATUS_DISCONNECTED = 0,            -- There is no activity to the server, this is the default value
+	STATUS_CONNECTING = 1,              -- We are trying to connect, we haven't got a clientID yet, we haven't been accepted by the server
+	STATUS_CONNECTED = 2,               -- The server has accepted us, we can talk and hear and we got a clientID, but we don't have the channels and clients yet, we can get server infos (welcome msg etc.)
+	STATUS_CONNECTION_ESTABLISHING = 3, -- we are CONNECTED and we are visible
+	STATUS_CONNECTION_ESTABLISHED = 4   -- we are CONNECTED and we have the client and channels available
+}
+]]
+
+	-- NOTES: I wish it gave a client # of what client had an event.  Right now when I get "a user disconnected" the OSD window is closing.  I don't see why that's happening.
+--[[
+-- disconnect
+osdModule: onConnectStatusChangeEvent: status=0, errorNumber=0, local error=0, myClientID=0
+-- connection sequence
+osdModule: onConnectStatusChangeEvent: status=1, errorNumber=0, local error=0, myClientID=0
+osdModule: onConnectStatusChangeEvent: status=2, errorNumber=0, local error=0, myClientID=11
+osdModule: onConnectStatusChangeEvent: status=3, errorNumber=0, local error=0, myClientID=11
+Connected to Server: "xxxx!"
+osdModule: onConnectStatusChangeEvent: status=4, errorNumber=0, local error=0, myClientID=11
+] ]
+--[[
+    ts3.printMessageToCurrentTab("osdModule: onConnectStatusChangeEvent: srv=" .. serverConnectionHandlerID .. " stat=" .. status .. " err=" .. errorNumber)
+	--os.execute("echo \"DEBUG TeamSpeak connect event " .. status .. "\" >> ~/Documents/ts3debug.log")
+
+	local myClientID, error = ts3.getClientID(serverConnectionHandlerID)
+	-- when disconnecting from the server kill the window (otherwise it will sit there on top until manually killed)
+	-- if getClientID returns a value then someone else disconnected, so don't kill the window
+	ts3.printMessageToCurrentTab("osdModule: onConnectStatusChangeEvent: status=" .. status .. ", errorNumber=" .. errorNumber .. ", local error=" .. error .. ", myClientID=" .. myClientID)
+	if (status==0 and myClientID==0) then
+		ts3.printMessageToCurrentTab("osdModule: onConnectStatusChangeEvent: killing OSD")
+		os.execute("pkill -TERM -f \"dzen2.*TeamSpeak\"")
+		return
+	else
+		-- Get name of this channel
+		local myChannelID, error = ts3.getChannelOfClient(serverConnectionHandlerID, myClientID)
+		local myChannelName, error = ts3.getChannelVariableAsString(serverConnectionHandlerID, myChannelID, ts3defs.ChannelProperties.CHANNEL_NAME)
+		if error == ts3errors.ERROR_ok and useChannelName==1 then
+			channelName = myChannelName
+		else
+			channelName = "TeamSpeak"
+		end
+	end
+end
+]]
+
+
 osdmodule_events = {
 	MenuIDs = MenuIDs,
 	moduleMenuItemID = moduleMenuItemID,
 	onMenuItemEvent = onMenuItemEvent,
-	onConnectStatusChangeEvent = onConnectStatusChangeEvent,
 	onClientMoveEvent = onClientMoveEvent,
 	onTalkStatusChangeEvent = onTalkStatusChangeEvent
 }
